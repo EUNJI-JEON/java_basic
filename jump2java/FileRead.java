96e900cc8c8743fd0aec05d2722f0b6a4b0249eb
eunjijeon <jej.career@gmail.com>
2019-06-25 16:48:22 +0900

    added some practice files

---
+++ jump2java/FileRead.java
@@ -0,0 +1,17 @@
+package jump2java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("c:/out.txt"));
+		while(true) {
+			String line = br.readLine();
+			if(line==null) break;
+			System.out.println(line);
+		}
+		br.close();
+	}
+}
--- jump2java/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module jump2java {
-}
